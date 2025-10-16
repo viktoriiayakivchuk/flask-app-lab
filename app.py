f6426dbaf1e36ce1f4afe02a9b221d3b2f83975c
ViktoriiaYakivchuk <viktoriia.yakivchuk.20@pnu.edu.ua>
2025-10-16 23:47:20 +0300

    feat: Complete Lab 2 with resume and contact pages

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return 'Hello, world!'
+def resume():
+    return render_template('resume.html', title="Моє резюме")
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html', title="Контакти")
 
 if __name__ == '__main__':
     app.run(debug=True)
