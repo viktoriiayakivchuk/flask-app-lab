bbf00f6af40eef6a1030282819a608d59449b645
ViktoriiaYakivchuk <viktoriia.yakivchuk.20@pnu.edu.ua>
2025-10-23 19:06:22 +0300

    feat: Initial project refactoring

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from flask import render_template, request
+from app import app  # Імпортуємо вже створену змінну 'app'
+
+@app.route('/')
+def main():
+    return render_template("base.html")
+
+@app.route('/homepage')
+def home():
+    """View for the Home page of your website."""
+    agent = request.user_agent
+    return render_template("home.html", agent=agent)
